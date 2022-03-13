10df41c89479ab07e7088b1b0d8bb8d0d1077e7c
Ahmed Badra <42746335+badra069@users.noreply.github.com>
2022-03-13 23:37:55 +0200

    added a project for timers driver

---
+++ timers-project/Debug/Src/dma.d
@@ -0,0 +1,13 @@
+Src/dma.o: ../Src/dma.c \
+ E:/My\ Workspace/C-sbme04t01-embedded-workspace/timers-project/Inc/MATH_macros.h \
+ E:/My\ Workspace/C-sbme04t01-embedded-workspace/timers-project/Inc/STD_types.h \
+ E:/My\ Workspace/C-sbme04t01-embedded-workspace/timers-project/Inc/rcc.h \
+ E:/My\ Workspace/C-sbme04t01-embedded-workspace/timers-project/Inc/dma.h
+
+E:/My\ Workspace/C-sbme04t01-embedded-workspace/timers-project/Inc/MATH_macros.h:
+
+E:/My\ Workspace/C-sbme04t01-embedded-workspace/timers-project/Inc/STD_types.h:
+
+E:/My\ Workspace/C-sbme04t01-embedded-workspace/timers-project/Inc/rcc.h:
+
+E:/My\ Workspace/C-sbme04t01-embedded-workspace/timers-project/Inc/dma.h:
---
+++ timers-project/Debug/Src/rcc.d
@@ -0,0 +1,10 @@
+Src/rcc.o: ../Src/rcc.c \
+ E:/My\ Workspace/C-sbme04t01-embedded-workspace/timers-project/Inc/MATH_macros.h \
+ E:/My\ Workspace/C-sbme04t01-embedded-workspace/timers-project/Inc/STD_types.h \
+ E:/My\ Workspace/C-sbme04t01-embedded-workspace/timers-project/Inc/rcc.h
+
+E:/My\ Workspace/C-sbme04t01-embedded-workspace/timers-project/Inc/MATH_macros.h:
+
+E:/My\ Workspace/C-sbme04t01-embedded-workspace/timers-project/Inc/STD_types.h:
+
+E:/My\ Workspace/C-sbme04t01-embedded-workspace/timers-project/Inc/rcc.h:
